b7b83d677414986dc5e9bee99ebd033cc44b15a5
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-11 20:33:08 +0900

    4. messages#create action

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,17 @@
 $(function(){
   $('#new_message').on('submit',function(e){
     e.preventDefault();
-    var input = $('#message_content').val();
-    console.log(input);
+    // var input = $('#message_content').val();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    // var url = window.location.href
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
