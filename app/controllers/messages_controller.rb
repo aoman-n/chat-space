e3857664e8ffc4299cad2a1682eb464116ff1637
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-12 19:06:50 +0900

    messages_controller fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,11 +14,12 @@ class MessagesController < ApplicationController
         format.html { redirect_to group_messages_path(@group) }
         format.json
       end
-      flash[:notice] = 'メッセージが送信されました'
     else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :index
+      # @messages = @group.messages.includes(:user)
+      # flash.now[:alert] = 'メッセージを入力してください。'
+      # @users = @group.users
+      # render 'messages/index'
+      render action: :index
     end
   end
 
