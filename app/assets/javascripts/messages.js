016104d9003b4cc44507c19c8bcbff4679dcaede
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-12 19:06:26 +0900

    Ajax

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,10 +1,32 @@
 $(function(){
+  function buildHTML(comment) {
+    // if(comment.image === null) {
+    //   var image = ""
+    // }else {
+    //   var image = `<img src= ${comment.image} >`
+    // }
+    var image = ((comment.image) ? `<img src= ${comment.image} >`: "");
+    var html =`<div class='comment'>
+                  <h4 class='comment__user-name'>
+                    ${comment.user_name}
+                  </h4>
+                  <ul class='comment__date clearfix'>
+                    <li>
+                      ${comment.created_at}
+                    </li>
+                  </ul>
+                  <p class='comment__text'>
+                    ${comment.content}
+                  </p>
+                    ${image}
+                </div>`
+    return html;
+  }
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
-    // var input = $('#message_content').val();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
-    // var url = window.location.href
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
@@ -12,6 +34,18 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main').append(html)
+      $('.message').val('');
+      $('.upload-icon').val('');
+      $('.sent-bottun').prop('disabled', false);
+      $('.chat-main').animate({scrollTop: $('.chat-main')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('コメントを入力してください');
+      $('.sent-bottun').prop('disabled', false);
     })
   });
 });
