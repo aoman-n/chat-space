59e62fba95aa95e68508e7e230b53da4e240d81c
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-14 13:04:03 +0900

    json or html in message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,10 @@ class MessagesController < ApplicationController
     @users = @group.users
     @messages = @group.messages.includes(:user)
     @message = Message.new
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
