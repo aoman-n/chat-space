5e672a429145dabf3b70605ed7aba4b86e776a63
first commit <>
2018-04-20 10:56:55 +0900

    unicorn_setting_fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.10.2'
 
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:<laster18>/<chat-space>.git'
